9656df6ec548794c9500c713179c7ce51be2bd2f
Jonathan Azoff <jon@azoffdesign.com>
2014-02-12 12:34:38 -1000

    Add personal access token

--- js/github.js
+++ js/github.js
@@ -2,10 +2,12 @@ define("github", ["jquery"], function($){
 
 	"use strict";
 
+	var token = "0ad7fe672ad82427e61e9633cd3876b0dbccd5cc";
+
 	return {
 
 		getRepoDetails: function(repoName) {
-			return $.getJSON("https://api.github.com/repos/"+repoName+"?callback=?");
+			return $.getJSON("https://api.github.com/repos/"+repoName+"?access_token="+token+"&callback=?");
 		},
 
 		statNode: function() {
