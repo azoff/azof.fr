5151d71fe697659672b7a1d32b6f41fcb07ca813
Jonathan Azoff <jon@azoffdesign.com>
2015-03-10 20:43:54 -0400

    remove oauth token

--- js/github.js
+++ js/github.js
@@ -2,12 +2,10 @@ define("github", ["jquery"], function($){
 
 	"use strict";
 
-	var token = "0ad7fe672ad82427e61e9633cd3876b0dbccd5cc";
-
 	return {
 
 		getRepoDetails: function(repoName) {
-			return $.getJSON("https://api.github.com/repos/"+repoName+"?access_token="+token+"&callback=?");
+			return $.getJSON("https://api.github.com/repos/"+repoName+"?callback=?");
 		},
 
 		statNode: function() {
